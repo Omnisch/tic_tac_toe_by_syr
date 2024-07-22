69772fcc10fa62a57ab3365894ff8ab71c715969
Omnisch <fleurdeln@gmail.com>
2024-07-22 23:08:08 +0800

    prepare for blindfold

---
+++ Assets/Scripts/Battle/Grid Tiles/BlindfoldTile.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Omnis.TicTacToe
+{
+    public class BlindfoldTile : GridTile
+    {
+        #region Fields
+        #endregion
+
+        #region Interfaces
+        #endregion
+
+        #region Functions
+        protected override void OnStart()
+        {
+            
+        }
+        #endregion
+    }
+}
